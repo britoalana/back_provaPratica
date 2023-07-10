adbd2d849924f6a9128d1c403dac464505a3b62e
britoalana <alana.brito@al.estudante.senai.br>
2023-07-10 18:21:15 -0300

    prova

---
+++ questoes/index.js
@@ -0,0 +1,51 @@
+// 1)
+//  const http = require('http')
+ 
+    
+// const server = http.createServer()
+// server.on((req, res)=>{
+   
+//     res.setHeader('Content-Type', 'application/json')
+//     const os = require('os')
+//     const itens= [ 
+//    (os.type()),
+//    (os.freemem()),
+//    (os.homedir()),
+// ]
+   
+// console.log(JSON.stringify(itens))
+// })
+ 
+
+
+// 2)
+// const path = require('path')
+ 
+// const caminho = ('https://al.senai.br/cursos/tecnico-em-informatica-para-internet/planodecurso.pdf')
+
+
+// console.log(`Nome do diretório: ${path.dirname(caminho)}`)
+// console.log(`Extensão do caminho: ${path.extname(caminho)}`)
+// console.log(`Caminho absoluto: ${path.resolve(caminho)}`)
+// console.log(path.parse(caminho))
+
+// 3)
+// const url = require('url')
+
+// url {
+//     pro
+// }
+
+// 4)
+// const fs = require('fs')
+
+// fs.rename('teste.txt', 'new.txt', (err)=>{
+//     if(err) throw(err)
+//     console.log('Arquivo renomeado')
+// })
+
+// 5) 
+
+// const OS = require('os')
+
+// console.log(OS.cpus())
